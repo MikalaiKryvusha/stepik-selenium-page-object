98ccfc378cf59e5f18a9db38726b6465fccf27c0
mikalai.kryvusha <mikalai.kryvusha@zorachka.com>
2021-12-18 12:45:41 +0300

    - is_element_present method added - little refactor

--- test_main_page.py
+++ test_main_page.py
@@ -6,3 +6,9 @@ def test_guest_can_go_to_login_page(browser):
     object_page_main = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     object_page_main.open()                      # открываем страницу
     object_page_main.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
